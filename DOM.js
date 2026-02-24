22da9712bdf88239027c163a86866cbfa42dc530
Suraj1215-coder <1215surajprajapatii1215@gmail.com>
2026-02-24 20:02:46 +0530

    first

---
+++ DOM.js
@@ -0,0 +1,14 @@
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me!";
+
+newBtn.style.color = "white";
+newBtn.style.backgroundColor = "red";
+
+document.querySelector("body").prepend(newBtn);
+
+// question 2
+
+let para = document.querySelector("p");
+
+
+
---
+++ Events In JS/event.js
@@ -0,0 +1,26 @@
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = () => {
+//     console.log("btn1 was clicked");
+//     let a = 23;
+//     a++;
+
+//     console.log(a);  // 24
+// };
+
+let modeBtn = document.querySelector("#mode");
+
+let currMode = "light";   // dark
+
+modeBtn.addEventListener("click", () =>{
+    if(currMode==="light"){
+        currMode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+    } else{
+        currMode = "light";
+      document.querySelector("body").style.backgroundColor = "light";
+
+    }
+    
+    console.log(currMode);
+});
