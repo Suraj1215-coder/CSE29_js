6d3a76e2bc6213d28689deca6321454efb614e9f
Suraj1215-coder <1215surajprajapatii1215@gmail.com>
2026-02-25 14:07:40 +0530

    first

--- FIRST PROGRAM/first.js
+++ FIRST PROGRAM/first.js
@@ -1,1 +1,27 @@
-console.log("hello surya");
+var num =10;
+
+console.log(num);
+
+console.log(typeof(num));
+
+num = false;
+
+console.log(num);
+
+console.log(typeof(num));
+
+num = null;
+
+
+const sym1 = symbol(4);
+console.log(sym1);
+
+const sym2 = symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log(true);
+}
+else{ 
+    console.log(false);
+}
+
