94ed954cc4c3a41112d851bf536673643957efe1
Suraj1215-coder <1215surajprajapatii1215@gmail.com>
2026-03-11 14:45:57 +0530

    function_2

---
+++ FIRST PROGRAM/function_2.js
@@ -0,0 +1,24 @@
+//object is the collection of anything
+
+var person = {
+    name : "john",
+    age: 30,
+    isMarried: true,
+
+    address: {
+        street : 'vinay nagar',
+        flatNo: 201
+    }
+
+}
+
+console.log(person)
+
+
+
+
+
+
+
+
+
